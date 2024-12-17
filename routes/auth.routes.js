a8f3d96e6bfeabb04bad3916fde8d7c6791a5450
Akash Wadgaonkar <akashbw01@gmail.com>
2024-12-17 10:21:37 +0530

    new dist added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,13 +6,13 @@
 //   loginCustomer,
 //   logoutCustomer,
 // } = require("../controllers/auth.controller");
-const { loginAdmin, logoutAdmin, registerCustomer, loginCustomer, logoutCustomer } = require("../controllers/auth.controller");
+const { loginAdmin, logoutAdmin, registerCustomer, loginCustomer, logoutCustomer, registerAdmin } = require("../controllers/auth.controller");
 
 const router = require("express").Router();
 
 // http://lcoalhost:5000/api/auth/admin/register
 router
-  // .post("/admin/register", registerAdmin)
+  .post("/admin/register", registerAdmin)
   .post("/admin/login", loginAdmin)
   .post("/admin/logout", logoutAdmin)
 
